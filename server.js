c3140f29838873998cc53d88539d5738e3a5e865
Sean <meyersean.c@gmail.com>
2019-01-05 16:34:40 -0600

    (feat) finish proxy for MVP

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 express = require("express");
 const app = express();
 const port = "3040";
+const cors = require("cors");
+app.use(cors());
+require("dotenv").config({ path: __dirname + "/../.env" });
 
 app.use(express.static("dist"));
 
